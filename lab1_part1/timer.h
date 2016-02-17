ee0476d11135ba2f0ec2f2b1097d08419b2d78a1
Joshua Johnston <jtjohnston@email.arizona.edu>
2016-02-16 20:53:32 -0700

    add correct files for part one to the repo

---
+++ lab1_part1/timer.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   timer.h
+ * Author: gvanhoy
+ *
+ * Created on August 27, 2015, 3:26 PM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+
+//void initTimer1();
+void initTimer2();
+void delayMs(unsigned int delay);
+//void resetT1();
+//void resetT2();
+
+
+#endif	/* TIMER_H */
+
