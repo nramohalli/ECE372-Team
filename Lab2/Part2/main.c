75598a257f8c23e2168208eb6e282e5007994d54
Joshua Johnston <jtjohnston@email.arizona.edu>
2016-02-29 12:04:21 -0700

    Changes to main

--- Lab2/Part2/main.c
+++ Lab2/Part2/main.c
@@ -65,49 +65,6 @@ case SETMODE:
                 break
         }           
     }
-//******************************************************************************************* //
-//
-// File:         lab1p2.c
-// Date:         12-30-2014
-// Authors:      Garrett Vanhoy
-//
-// Description: 
-// ******************************************************************************************* //
-
-#include <xc.h>
-#include <sys/attribs.h>
-#include "lcd.h"
-#include "timer.h"
-#include "config.h"
-#include "interrupt.h"
-#include "led.h"
-
-#define DELAY_TIME 100
-
-//TODO: Define states of the state machine
-typedef enum stateTypeEnum{
-    
-} stateType;
-
-
-// ******************************************************************************************* //
-
-int main(void)
-{
-    enableInterrupts();                   //This function is necessary to use interrupts.
-
-	initLEDs();
-	initTimer1();
-	initTimer2();
-	initSW();
-	initLCD();
-    
-    //init state var
-    stateType state = INIT;
-    
-    SYSTEMConfigPerformance(10000000);
-    while(1)
-    {      }
     
     return 0;
 }
@@ -121,7 +78,4 @@ void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterupt(){
     
 // debounce
     delayUs(DELAY_TIME);
-// add letter to lcd
 }
-    
-    
